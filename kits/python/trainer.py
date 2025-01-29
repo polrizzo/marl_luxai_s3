c1dcd0fcbe3f8bd8a0784ede85f5b512c2c753a7
polrizzo <paolo.rizzo.ds@gmail.com>
2025-01-29 15:54:08 +0100

    Fix modularity of eval/optim hyperparameters

--- kits/python/trainer.py
+++ kits/python/trainer.py
@@ -143,12 +143,12 @@ if __name__ == "__main__":
         wandb.log({"total_games": i, "seed": seed,
                    "winner": 1 if winner > 0 else -1})
         # Update target_net every 1/100 of num_games
-        if (i+1) % (config_trainer["hyper"]['num_games'] // 100) == 0:
+        if (i+1) % (config_trainer["hyper"]['num_games'] // config_trainer["hyper"]['update_tn']) == 0:
             player_0.update_target_net()
             player_1.update_target_net()
 
         # Eval phase every 1/10 of num_games
-        if (i + 1) % (config_trainer["hyper"]['num_games'] // 10) == 0:
+        if (i + 1) % (config_trainer["hyper"]['num_games'] // config_trainer["hyper"]['eval']) == 0:
         # if i  == 0:
             game_done = False
             last_obs = None
@@ -194,7 +194,7 @@ if __name__ == "__main__":
                 if dones["player_0"] or dones["player_1"]:
                     game_done = True
                     print(obs["player_0"]["team_wins"])
-            game_num = (i + 1) // (config_trainer["hyper"]['num_games'] // 10)
+            game_num = (i + 1) // (config_trainer["hyper"]['num_games'] // config_trainer["hyper"]['eval'])
             name_eval = "./replays/game_" + str(game_num) + ".json"
             env_eval.save_episode(save_path=name_eval)
             env_eval.close()
