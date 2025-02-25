42b4bc2b2179f4b2d9cc28f9a01935e9cbda60cc
polrizzo <paolo.rizzo.ds@gmail.com>
2025-02-25 12:58:14 +0100

    Fix import file for ssh launch

--- kits/python/trainer.py
+++ kits/python/trainer.py
@@ -8,7 +8,7 @@ import yaml
 import numpy as np
 
 from state_custom import update_single_unit_energy, global_state
-from luxai_s3.wrappers import RecordEpisode, LuxAIS3GymEnv
+from src.luxai_s3.wrappers import RecordEpisode, LuxAIS3GymEnv
 from agent_rl import AgentRl
 from reward import get_global_reward, get_unit_reward
 
